838e3d7e5e6e13e16966a5bc6d07a2470c9da1b8
revdancatt <github@revdancatt.com>
2018-05-08 14:40:05 +0100

    Start pinging graphQL server

--- src/modules/pingtools/index.js
+++ src/modules/pingtools/index.js
@@ -1,10 +1,40 @@
-const pingGraphQL = () => {
-  console.log('Ping GraphQL')
+const Queries = require('../queries')
+
+const pingGraphQL = async () => {
+  const queries = new Queries()
+  const payload = {
+    query: queries.get('hello', '')
+  }
+  const ping = {}
+  const startms = new Date().getTime()
+  const results = await queries.fetch(payload)
+  const endms = new Date().getTime()
+  ping.ms = endms - startms
+  ping.timestamp = endms
+  //  If we got an array back, it means we had an error
+  //  and we should count that as a miss, otherwise assume
+  //  all is good
+  if (Array.isArray(results)) {
+    ping.valid = false
+  } else {
+    ping.valid = true
+  }
+  //  Pop the updated information into the global array. Note
+  //  to start with this will clear out each time we refresh
+  //  the server, which _shouldn't_ be so much of a problem when
+  //  the system goes live, as we're not planning on restarting
+  //  it that often anyway. And it isn't that critical if we don't
+  //  have that much data anyway
+  if (!('graphqlping' in global)) {
+    global.graphqlping = []
+  }
+  global.graphqlping.unshift(ping)
+  global.graphqlping = global.graphqlping.slice(0, 30)
 }
 
 exports.startPinging = () => {
   //  Ping GrahpQL
-  global.pingGraphQLTmr = setTimeout(() => {
+  global.pingGraphQLTmr = setInterval(() => {
     pingGraphQL()
   }, 60 * 1000)
   pingGraphQL()
