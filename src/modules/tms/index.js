11eac425800c2e57a2dcdf2b4a789ef131c0bd0b
revdancatt <github@revdancatt.com>
2018-05-24 13:34:50 +0100

    Write objects into a process folder

--- src/modules/tms/index.js
+++ src/modules/tms/index.js
@@ -79,6 +79,28 @@ const saveImageSource = (stub, id, source) => {
     perfectFile.remote = null
     perfectFileJSONPretty = JSON.stringify(perfectFile, null, 4)
     fs.writeFileSync(filename, perfectFileJSONPretty, 'utf-8')
+
+    //  We also need to move the file from the 'processed' folder into the process
+    //  folder if it exists, to make it trigger a new upload
+    const sourceObjectJSON = path.join(rootDir, 'tms', stub, 'processed', subFolder, `${id}.json`)
+    if (fs.existsSync(sourceObjectJSON)) {
+      tmsLogger.object(`Remove ${id} for ${stub} from processed folder and putting into process`, {
+        action: 'modified',
+        id: id,
+        stub: stub
+      })
+      //  Make sure the directory we are trying to copy into exists
+      //  (in theory of course it should, but you know!)
+      if (!fs.existsSync(path.join(rootDir, 'tms', stub, 'process'))) {
+        fs.mkdirSync(path.join(rootDir, 'tms', stub, 'process'))
+      }
+      if (!fs.existsSync(path.join(rootDir, 'tms', stub, 'process', subFolder))) {
+        fs.mkdirSync(path.join(rootDir, 'tms', stub, 'process', subFolder))
+      }
+      const targetObjectJSON = path.join(rootDir, 'tms', stub, 'process', subFolder, `${id}.json`)
+      //  Move the file over
+      fs.renameSync(sourceObjectJSON, targetObjectJSON)
+    }
   }
 }
 
